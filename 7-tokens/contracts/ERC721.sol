a8af40714b14fb9c65e3779dd8ca2f8030e1a6c4
Olorunshogo Moses BAMTEFA <shownzy001@gmail.com>
2026-02-18 14:32:41 +0100

    so far 2 - erc721 token

--- 7-tokens/contracts/ERC721.sol
+++ 7-tokens/contracts/ERC721.sol
@@ -15,12 +15,12 @@ interface IERC721 is IERC165 {
   function balanceOf(address owner) external view returns (uint256);
   function ownerOf(uint256 tokenId) external view returns (address);
 
-  function safeTransferFrom(address from, address to, uint256 tokenId) external;
-  function safeTransferFrom (address from, address to, uint256 tokenId, bytes calldata data) external;
+  // function safeTransferFrom(address from, address to, uint256 tokenId) external;
+  // function safeTransferFrom (address from, address to, uint256 tokenId, bytes calldata data) external;
 
-  function transferFrom(address from, address to, uint256 _tokenId) external payable;
+  function transferFrom(address from, address to, uint256 _tokenId) external;
 
-  function approve(address to, uint256 tokenId) external payable;
+  function approve(address to, uint256 tokenId) external;
   function setApprovalForAll(address operator, bool approved) external;
 
   function getApproved(uint256 tokenId) external view returns (address);
@@ -40,7 +40,7 @@ interface IERC721Receiver {
     function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns (bytes4);
 }
 
-abstract contract ERC721 is IERC721Metadata {
+contract ERC721 is IERC721Metadata {
   string private _name;
   string private _symbol;
   string private _baseURI;
@@ -61,7 +61,7 @@ abstract contract ERC721 is IERC721Metadata {
 
   function supportInterface(bytes4 interfaceId) external pure returns (bool) {
     return interfaceId == 0x01ffc9a7 || // IERC 165
-        interfaceId == 0x80ac || // IERC721
+        interfaceId == 0x80ac58cd || // IERC721
         interfaceId == 0x5b5e139f; // IERC721Metadata
   }
 
@@ -79,7 +79,7 @@ abstract contract ERC721 is IERC721Metadata {
     return _balances[owner];
   }
 
-  function ownerOf(uint256 tokenId) external view returns (address) {
+  function ownerOf(uint256 tokenId) public view returns (address) {
     address owner = _owners[tokenId];
     require(owner != address(0), "Zero address");
     return owner;
@@ -94,6 +94,90 @@ abstract contract ERC721 is IERC721Metadata {
     if(bytes(_baseURI).length == 0) return "";
     return string(abi.encodePacked(_baseURI, tokenId));
   }
+
+  function approve(address to, uint256 tokenId) external {
+    address owner = ownerOf(tokenId);
+    require(to != owner, "Cannot approve to current owner");
+    require(msg.sender == owner, "Unauthorized");
+    _tokenApprovals[tokenId] = to;
+    emit Approval(owner, to, tokenId);
+  }
+
+  function getApproved(uint256 tokenId) public view returns (address) {
+    require(_exists(tokenId), "Token does not exist");
+    return _tokenApprovals[tokenId];
+  }
+
+  function setApprovalForAll(address operator, bool approved) external {
+    require(operator != msg.sender, "Cannot approve to self");
+    _operatorApprovals[msg.sender][operator] = approved;
+    emit ApprovalForAll(msg.sender, operator, approved);
+  }
+
+  function isApprovedForAll(address owner, address operator) public view returns (bool) {
+    return _operatorApprovals[owner][operator];
+  }
+
+  function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
+    address owner = ownerOf(tokenId);
+    return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+  }
+
+  function _transfer(address from, address to, uint256 tokenId) internal {
+    delete _tokenApprovals[tokenId];
+    emit Approval(from, address(0), tokenId);
+
+    _balances[from] -= 1;
+    _balances[to] += 1;
+    _owners[tokenId] = to;
+
+    emit Transfer(from, to, tokenId);
+  }
+
+  // function _checkOnERC721Received(address operator, address from, address to, uint256 tokenId, bytes memory data) internal returns (bool) {
+
+  // }
+
+  function transferFrom(address from, address to, uint tokenId) public {
+    require(_isApprovedOrOwner(msg.sender, tokenId), "Unauthorized");
+    require(ownerOf(tokenId) == from, "Wrong from");
+    require(to != address(0), "Transfer to zero address");
+    _transfer(from, to, tokenId);
+  }
+
+  // Mint
+  function _mint(address to, uint256 tokenId) internal {
+    require(to != address(0), "Mint to zero address");
+    require(!_exists(tokenId), "Token already minted");
+
+    _balances[to] += 1;
+    _owners[tokenId] = to;
+
+    emit Transfer(address(0), to, tokenId);
+  }
+
+  function mint(address to, uint256 tokenId) external {
+    _mint(to, tokenId);
+  }
+
+  function setBaseURI(string calldata newBaseURI) external {
+    _baseURI = newBaseURI;
+  }
+
+  function burn(uint256 tokenId) external {
+    address owner = ownerOf(tokenId);
+    require(_isApprovedOrOwner(msg.sender, tokenId), "unauthorized");
+
+    delete _tokenApprovals[tokenId];
+    emit Approval(owner, address(0), tokenId);
+
+    _balances[owner] -= 1;
+    delete _owners[tokenId];
+
+    emit Transfer(owner, address(0), tokenId);
+  }
+
+
 
 }
 
