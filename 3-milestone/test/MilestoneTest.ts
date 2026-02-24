5b76f648b7aae03d4d59536e65b459f0083efc97
Olorunshogo Moses BAMTEFA <shownzy001@gmail.com>
2026-02-24 14:07:13 +0100

    chore: renamed FactoryEscrowTest.ts to 3-milestone/test/milestoneTest.ts

--- 3-milestone/test/MilestoneTest.ts
+++ 3-milestone/test/MilestoneTest.ts
@@ -3,9 +3,9 @@ import { network } from 'hardhat';
 
 const { ethers, networkHelpers } = await network.connect();
 
-describe('EscrowFactory Contract', function () {
+describe('MilestoneFactory Contract', function () {
   let factory: any;
-  let escrow: any;
+  let milestone: any;
 
   let client: any;
   let freelancer: any;
@@ -20,14 +20,12 @@ describe('EscrowFactory Contract', function () {
   beforeEach(async () => {
     [client, freelancer, other] = await ethers.getSigners();
 
-    factory = await ethers.deployContract('EscrowFactory');
+    factory = await ethers.deployContract('MilestoneFactory');
     await factory.waitForDeployment();
   });
 
-  // Create Escrow
-
   describe('createEscrow', () => {
-    it('Should create escrow and emit EscrowCreated', async () => {
+    it('Should create milestone escrow and emit EscrowCreated', async () => {
       await expect(
         factory
           .connect(client)
@@ -71,8 +69,7 @@ describe('EscrowFactory Contract', function () {
     });
   });
 
-  // Escrow Behviour
-  describe('StandardEscrow Integration', () => {
+  describe('Milestone Integration', () => {
     beforeEach(async () => {
       await factory
         .connect(client)
@@ -86,117 +83,104 @@ describe('EscrowFactory Contract', function () {
 
       const escrowAddress = await factory.escrows(0);
 
-      escrow = await ethers.getContractAt('StandardEscrow', escrowAddress);
+      milestone = await ethers.getContractAt('Milestone', escrowAddress);
     });
 
-    it('Should initialize escrow correctly', async () => {
-      expect(await escrow.client()).to.equal(client.address);
-      expect(await escrow.freelancer()).to.equal(freelancer.address);
-      expect(await escrow.totalMilestones()).to.equal(totalMilestones);
-      expect(await escrow.milestoneAmount()).to.equal(milestoneAmount);
-      expect(await escrow.status()).to.equal(0); // ACTIVE
+    it('Should initialize milestone correctly', async () => {
+      expect(await milestone.client()).to.equal(client.address);
+      expect(await milestone.freelancer()).to.equal(freelancer.address);
+      expect(await milestone.totalMilestones()).to.equal(totalMilestones);
+      expect(await milestone.milestoneAmount()).to.equal(milestoneAmount);
+      expect(await milestone.status()).to.equal(0); // ACTIVE
     });
-
-    /* =============================================================
-                        MILESTONE FLOW
-    ============================================================= */
 
     it('Freelancer can mark milestone completed', async () => {
-      await escrow.connect(freelancer).markMilestoneCompleted();
-
-      expect(await escrow.completedMilestones()).to.equal(1);
+      await milestone.connect(freelancer).markMilestoneCompleted();
+      expect(await milestone.completedMilestones()).to.equal(1);
     });
 
     it('Should revert if non-freelancer marks milestone', async () => {
       await expect(
-        escrow.connect(other).markMilestoneCompleted()
+        milestone.connect(other).markMilestoneCompleted()
       ).to.be.revertedWith('Only freelancer');
     });
 
     it('Client can approve milestone and release payment', async () => {
-      await escrow.connect(freelancer).markMilestoneCompleted();
+      await milestone.connect(freelancer).markMilestoneCompleted();
 
       const balanceBefore = await ethers.provider.getBalance(
         freelancer.address
       );
 
-      await escrow.connect(client).approveMilestone();
+      await milestone.connect(client).approveMilestone();
 
-      const balanceAfter = await ethers.provider.getBalance(freelancer.address);
+      const balanceAfter = await ethers.provider.getBalance(
+        freelancer.address
+      );
 
       expect(balanceAfter - balanceBefore).to.equal(milestoneAmount);
-      expect(await escrow.releasedMilestones()).to.equal(1);
+      expect(await milestone.releasedMilestones()).to.equal(1);
     });
 
     it('Should revert if non-client tries to approve', async () => {
-      await escrow.connect(freelancer).markMilestoneCompleted();
+      await milestone.connect(freelancer).markMilestoneCompleted();
 
-      await expect(escrow.connect(other).approveMilestone()).to.be.revertedWith(
-        'Only client'
-      );
+      await expect(
+        milestone.connect(other).approveMilestone()
+      ).to.be.revertedWith('Only client');
     });
 
     it('Should not release payment if no completed milestone', async () => {
       await expect(
-        escrow.connect(client).approveMilestone()
+        milestone.connect(client).approveMilestone()
       ).to.be.revertedWith('Nothing to release');
     });
 
-    /* =============================================================
-                        TIMEOUT LOGIC
-    ============================================================= */
-
     it('Freelancer can claim timeout payment', async () => {
-      await escrow.connect(freelancer).markMilestoneCompleted();
+      await milestone.connect(freelancer).markMilestoneCompleted();
 
       await networkHelpers.time.increase(approvalTimeout + 1);
 
       const balanceBefore = await ethers.provider.getBalance(
         freelancer.address
       );
-      console.log(`Balance before is: ${balanceBefore}`)
 
-      await escrow.connect(freelancer).claimTimeoutPayment();
+      await milestone.connect(freelancer).claimTimeoutPayment();
 
-      const balanceAfter = await ethers.provider.getBalance(freelancer.address);
-      console.log(`Balance after is: ${balanceAfter}`);
-      const difference = balanceAfter - balanceBefore;
-      console.log(`Difference between after and before is: ${balanceAfter} `);
+      const balanceAfter = await ethers.provider.getBalance(
+        freelancer.address
+      );
 
-      // expect(difference).to.equal(milestoneAmount);
-      expect(await escrow.releasedMilestones()).to.equal(1);
+      expect(await milestone.releasedMilestones()).to.equal(1);
+      expect(balanceAfter - balanceBefore).to.equal(milestoneAmount);
     });
 
     it('Should revert timeout claim if timeout not reached', async () => {
-      await escrow.connect(freelancer).markMilestoneCompleted();
+      await milestone.connect(freelancer).markMilestoneCompleted();
 
       await expect(
-        escrow.connect(freelancer).claimTimeoutPayment()
+        milestone.connect(freelancer).claimTimeoutPayment()
       ).to.be.revertedWith('Timeout not reached');
     });
 
-    /* =============================================================
-                        FULL COMPLETION
-    ============================================================= */
-
     it('Should complete contract after all milestones paid', async () => {
       for (let i = 0; i < totalMilestones; i++) {
-        await escrow.connect(freelancer).markMilestoneCompleted();
-        await escrow.connect(client).approveMilestone();
+        await milestone.connect(freelancer).markMilestoneCompleted();
+        await milestone.connect(client).approveMilestone();
       }
 
-      expect(await escrow.status()).to.equal(1); // COMPLETE
-      expect(await escrow.releasedMilestones()).to.equal(totalMilestones);
+      expect(await milestone.status()).to.equal(1); // COMPLETE
+      expect(await milestone.releasedMilestones()).to.equal(totalMilestones);
     });
 
     it('Should revert if marking milestone after completion', async () => {
       for (let i = 0; i < totalMilestones; i++) {
-        await escrow.connect(freelancer).markMilestoneCompleted();
-        await escrow.connect(client).approveMilestone();
+        await milestone.connect(freelancer).markMilestoneCompleted();
+        await milestone.connect(client).approveMilestone();
       }
 
       await expect(
-        escrow.connect(freelancer).markMilestoneCompleted()
+        milestone.connect(freelancer).markMilestoneCompleted()
       ).to.be.revertedWith('Not active');
     });
   });
